49472e9f7b7fd68eca360ca3ba4a3b0e2b8bbc5d
azseza <azeer.ltifi@gmail.com>
2021-04-21 01:51:53 +0100

    Added hand threashHold

--- handCounterHelper.py
+++ handCounterHelper.py
@@ -41,4 +41,56 @@ def read_the_imagez():
 #Recongnizing the gestures 
 
 def recognize(frame: np.uint8)->Tuple[int, np.ndarray]:
-    
+    """Python function to recognize how many fingers are 
+    in the picture
+
+    Args:
+        frame (np.uint8): Input gray scaled image
+
+    Returns:
+        Tuple[int, np.ndarray]: number of fingers, countour of the hand
+    """
+    segment = segment_arm(frame)
+    contour, defects = find_hull_effects(segment)
+    image_draw = cv.cvtColor(segment, cv.COLOR_GRAY2RGB)
+    num_fingers, image_draw = detect_num_fingers(contour, defects, image_draw)
+    return num_fingers, image_draw
+
+
+
+def segment_arm(frame: np.uint8, absolute_depth: int = 14) -> np.ndarray:
+    """Function that segments an image and returns a black and white image
+    smoothed image of the hand to count
+
+    Args:
+        frame (np.uint8): [description]
+        absolute_depth (int, optional): [description]. Defaults to 14.
+
+    Returns:
+        np.ndarray: [description]
+    """
+    height, width = frame.shape[:2]
+    #Finding the center region of imgheight frame 
+    center_half = 10 # half-width of 21 is 21/2-1
+    center = frame[height // 2 - center_half:height // 2 + center_half, 
+                   width // 2 - center_half:width // 2 +center_half]
+    #putting the filter border
+    median = np.median(center)
+    #applying the first filter in grayscale(unknown region nor hand nor non-hand)
+    frame = np.where(abs(frame-median) <= absolute_depth, 128, 0).astype(np.uint8)
+    #morphological closing and smoothing
+    kernel = np.ones((3, 3), np.uint8)
+    frame = cv.morphologyEx(frame, cv.MORPH_CLOSE, kernel).astype(np.ndarray)
+    #applying white filter to the hand (part we are sure about it's the hand) 
+    smaal_kernel = 3 
+    frame[height // 2 - smaal_kernel:height // 2 + smaal_kernel,
+         width // 2 - smaal_kernel:width // 2 + smaal_kernel] = 128
+    #Applapplying the mask 
+    mask = np.zeros((height + 2, width + 2), np.uint8)    
+    flood = frame.copy()
+    cv.floodFilld(flood, mask, (width // 2, height // 2), 255, flags=4 | (255<<8))
+    ret, flooded = cv.threshold(flood, 129, 255, cv.THRESH_BINARY)
+    return flooded
+
+
+        
